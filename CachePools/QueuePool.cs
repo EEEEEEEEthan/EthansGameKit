e30570859890b80ee6eb6d839e4b61e15870676f
Ethan <tyx1993@live.cn>
2023-03-09 01:40:17 +0800

    pool

---
+++ CachePools/QueuePool.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+namespace EthansGameKit.CachePools
+{
+	public static class QueuePool<T>
+	{
+		static readonly CachePool pool = new(0);
+		public static void ClearAndRecycle(ref Queue<T> queue)
+		{
+			var obj = (object)queue;
+			queue.Clear();
+			pool.Recycle(ref obj);
+			queue = null;
+		}
+		public static Queue<T> Generate()
+		{
+			if (pool.TryGenerate(out var cache))
+				return (Queue<T>)cache;
+			return new();
+		}
+	}
+}
