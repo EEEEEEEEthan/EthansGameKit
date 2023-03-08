a76c093f4eee2edf377ea9585dfc16274f7f9cb7
ethan <ethan.tang@coconutislandstudio.com>
2023-03-08 16:14:56 +0800

    fakeSingleton

--- FakeSingleton.cs
+++ FakeSingleton.cs
@@ -3,20 +3,32 @@ using UnityEngine.Assertions;
 
 namespace EthansGameKit
 {
-    [DefaultExecutionOrder(int.MinValue)]
-    public class FakeSingleton<T> : MonoBehaviour where T : FakeSingleton<T>
-    {
-        public static T Instance { get; private set; }
-        protected void OnEnable()
-        {
+	[DefaultExecutionOrder(int.MinValue)]
+	public class FakeSingleton<T> : MonoBehaviour where T : FakeSingleton<T>
+	{
+		public static T Instance { get; private set; }
+		protected void OnEnable()
+		{
 #if UNITY_EDITOR
-            Assert.IsFalse(Instance, $"duplicated instance {Instance}");
+			Assert.IsFalse(Instance, $"duplicated instance {Instance}");
 #endif
-            Instance = (T)this;
-        }
-        protected void OnDisable()
-        {
-            Instance = null;
-        }
-    }
+			Instance = (T)this;
+		}
+		protected void OnDisable()
+		{
+			Instance = null;
+		}
+	}
+	public interface IFakeSingleton<T> where T : MonoBehaviour, IFakeSingleton<T>
+	{
+		static T instance;
+		public static T Instance
+		{
+			get
+			{
+				if (instance) return instance;
+				return instance = Object.FindObjectOfType<T>();
+			}
+		}
+	}
 }
