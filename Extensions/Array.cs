e35b3518dd4406b6f54a7a5c3063ec7c856a3cfd
ethan <ethan.tang@coconutislandstudio.com>
2023-03-09 18:46:41 +0800

    extensions

---
+++ Extensions/Array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// ReSharper disable once CheckNamespace
+namespace EthansGameKit
+{
+	public static partial class Extensions
+	{
+		public static void MemSet<T>(this T[] @this, int index, int length, T value)
+		{
+			if (index < 0 || index >= @this.Length) throw new ArgumentOutOfRangeException(nameof(index));
+			if (length < 0 || index + length > @this.Length) throw new ArgumentOutOfRangeException(nameof(length));
+			if (length <= 0) return;
+			@this[index] = value;
+			var copiedLength = 1;
+			while (copiedLength < length)
+			{
+				var remainingLength = length - copiedLength;
+				var copyLength = Math.Min(remainingLength, copiedLength);
+				Array.Copy(@this, index, @this, index + copiedLength, copyLength);
+				copiedLength += copyLength;
+			}
+		}
+	}
+}
