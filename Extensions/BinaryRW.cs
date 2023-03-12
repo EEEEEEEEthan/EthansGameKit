8d2e4813a77cc7518f26e2ea82faf95502a026cf
Ethan <tyx1993@live.cn>
2023-03-13 00:56:31 +0800

    extensions

---
+++ Extensions/BinaryRW.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace EthansGameKit
+{
+	public static partial class Extensions
+	{
+		public static bool Write(this BinaryWriter @this, Vector3 vector)
+		{
+			@this.Write(vector.x);
+			@this.Write(vector.y);
+			@this.Write(vector.z);
+			return true;
+		}
+		public static Vector3 ReadVector3(this BinaryReader @this)
+		{
+			return new(@this.ReadSingle(), @this.ReadSingle(), @this.ReadSingle());
+		}
+		public static bool Write(this BinaryWriter @this, Quaternion quaternion)
+		{
+			@this.Write(quaternion.x);
+			@this.Write(quaternion.y);
+			@this.Write(quaternion.z);
+			@this.Write(quaternion.w);
+			return true;
+		}
+		public static Quaternion ReadQuaternion(this BinaryReader @this)
+		{
+			return new(@this.ReadSingle(), @this.ReadSingle(), @this.ReadSingle(), @this.ReadSingle());
+		}
+	}
+}
