e8ab2e9adc310fa4d5a17979b3bda810c5facbaf
ethan <ethan.tang@coconutislandstudio.com>
2023-03-31 14:29:30 +0800

    refreshableItem

---
+++ IRefreshableItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace EthansGameKit
+{
+	public interface IRefreshableItem
+	{
+		void Refresh(bool immediate)
+		{
+			if (immediate)
+			{
+				RefreshableItemManager.dirtyItems.Remove(this);
+				try
+				{
+					OnRefresh();
+				}
+				catch (Exception e)
+				{
+					Debug.LogException(e);
+				}
+			}
+			else
+			{
+				RefreshableItemManager.dirtyItems.Add(this);
+			}
+		}
+		protected internal void OnRefresh();
+	}
+}
---
+++ Internal/RefreshableItemManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EthansGameKit
+{
+	class RefreshableItemManager : MonoBehaviour
+	{
+		internal static readonly HashSet<IRefreshableItem> dirtyItems = new();
+		static IRefreshableItem[] buffer = Array.Empty<IRefreshableItem>();
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+		static void Initialize()
+		{
+			var gameObject = new GameObject(nameof(RefreshableItemManager));
+			gameObject.AddComponent<RefreshableItemManager>();
+			DontDestroyOnLoad(gameObject);
+		}
+		void Update()
+		{
+			var count = dirtyItems.Count;
+			if (count <= 0) return;
+			if (buffer.Length < count)
+				buffer = new IRefreshableItem[count];
+			dirtyItems.CopyTo(buffer);
+			dirtyItems.Clear();
+			for (var i = 0; i < count; i++)
+			{
+				var item = buffer[i];
+				try
+				{
+					item.OnRefresh();
+				}
+				catch (Exception e)
+				{
+					Debug.LogException(e);
+				}
+			}
+		}
+	}
+}
