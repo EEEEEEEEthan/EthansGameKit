a53b4cfb67d00f80279d5b84bd64ca84cdceda40
Ethan <tyx1993@live.cn>
2023-04-02 18:23:52 +0800

    code optmize

--- IRefreshableItem.cs
+++ IRefreshableItem.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using EthansGameKit.Internal;
 using UnityEngine;
 
 namespace EthansGameKit
--- Internal/RefreshableItemManager.cs
+++ Internal/RefreshableItemManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace EthansGameKit
+namespace EthansGameKit.Internal
 {
 	class RefreshableItemManager : MonoBehaviour
 	{
