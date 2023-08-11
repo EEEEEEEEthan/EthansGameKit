60edf2211e0732a49b0f9814399eb8c752a1a3b0
Ethan <tyx1993@live.cn>
2023-08-11 22:00:28 +0800

    smoothedTransform 接口丰富化

--- Components/SmoothedTransform.cs
+++ Components/SmoothedTransform.cs
@@ -32,5 +32,63 @@ namespace EthansGameKit.Components
 				transform.localRotation = rotation.Value;
 			lastRotation = transform.localRotation;
 		}
+		public void Shift(Vector3 offset)
+		{
+			position.PreferredValue += offset;
+		}
+		public void MoveTo(Vector3 position)
+		{
+			this.position.PreferredValue = position;
+		}
+		public void RotateTowards(Quaternion rotation)
+		{
+			this.rotation.PreferredValue = rotation;
+		}
+		public void RotateTowards(Vector3 forward)
+		{
+			RotateTowards(Quaternion.LookRotation(forward));
+		}
+		public void RotateTowards(Vector3 forward, Vector3 up)
+		{
+			RotateTowards(Quaternion.LookRotation(forward, up));
+		}
+		public void LookAt(Vector3 position)
+		{
+			RotateTowards(Quaternion.LookRotation(position - this.position.PreferredValue));
+		}
+		public void LookAt(Vector3 position, Quaternion rotation)
+		{
+			RotateTowards(rotation);
+			LookAt(position);
+		}
+		public void LookAt(Vector3 position, Vector3 forward)
+		{
+			RotateTowards(forward);
+			LookAt(position);
+		}
+		public void LookAt(Vector3 position, Vector3 forward, Vector3 up)
+		{
+			RotateTowards(forward, up);
+			LookAt(position);
+		}
+		public void LookAt(Vector3 position, float distance)
+		{
+			MoveTo(position - rotation.PreferredValue.Forward() * distance);
+		}
+		public void LookAt(Vector3 position, Quaternion rotation, float distance)
+		{
+			RotateTowards(rotation);
+			LookAt(position, distance);
+		}
+		public void LookAt(Vector3 position, Vector3 forward, float distance)
+		{
+			RotateTowards(forward);
+			LookAt(position, distance);
+		}
+		public void LookAt(Vector3 position, Vector3 forward, Vector3 up, float distance)
+		{
+			RotateTowards(forward, up);
+			LookAt(position, distance);
+		}
 	}
 }
