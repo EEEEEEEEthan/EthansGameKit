80c75ca002422e361fac1b7badb1990e0fa0c201
Ethan <tyx1993@live.cn>
2023-08-16 00:29:09 +0800

    1

--- Components/SmoothedCamera.cs
+++ Components/SmoothedCamera.cs
@@ -8,11 +8,14 @@ namespace EthansGameKit.Components
 	{
 		[SerializeField] SmoothedSingle fieldOfView = new();
 		[SerializeField] SmoothedSingle orthographicSize = new();
+		[SerializeField] SmoothedSingle focusDistance = new();
 		float lastFieldOfView;
 		float lastOrthographicSize;
+		float lastFocusDistance;
 		public Camera TargetCamera { get; private set; }
 		public SmoothedSingle FieldOfView => fieldOfView;
 		public SmoothedSingle OrthographicSize => orthographicSize;
+		public SmoothedSingle FocusDistance => focusDistance;
 		void OnEnable()
 		{
 			TargetCamera = GetComponent<Camera>();
@@ -29,16 +32,24 @@ namespace EthansGameKit.Components
 				orthographicSize.Value = TargetCamera.orthographicSize;
 				return;
 			}
+			// field of view
 			if (lastFieldOfView != TargetCamera.fieldOfView)
 				fieldOfView.Value = TargetCamera.fieldOfView;
 			else
 				TargetCamera.fieldOfView = fieldOfView.Value;
 			lastFieldOfView = TargetCamera.fieldOfView;
+			// orthographic size
 			if (lastOrthographicSize != TargetCamera.orthographicSize)
 				orthographicSize.Value = TargetCamera.orthographicSize;
 			else
 				TargetCamera.orthographicSize = orthographicSize.Value;
 			lastOrthographicSize = TargetCamera.orthographicSize;
+			// focus distance
+			if (lastFocusDistance != TargetCamera.focusDistance)
+				focusDistance.Value = TargetCamera.focusDistance;
+			else
+				TargetCamera.focusDistance = focusDistance.Value;
+			lastFocusDistance = TargetCamera.focusDistance;
 		}
 	}
 }
