3b861095ec9cda2348dba01a03106e7d742ef8d2
Ethan <tyx1993@live.cn>
2023-09-11 22:08:24 +0800

    GetOrAddComponent

--- Scripts/Extensions/GameObjectExtensions.cs
+++ Scripts/Extensions/GameObjectExtensions.cs
@@ -25,5 +25,22 @@ namespace EthansGameKit
 			component = @this.GetComponentInChildren<T>(false);
 			return component != null;
 		}
+		public static T GetOrAddComponent<T>(this GameObject @this, bool withUndo = false) where T : Component
+		{
+			var component = @this.GetComponent<T>();
+			if (!component)
+			{
+				#if UNITY_EDITOR
+				if (withUndo)
+				{
+					UnityEditor.Undo.RecordObject(@this, $"Add Component {typeof(T).Name}");
+					component = @this.AddComponent<T>();
+					UnityEditor.EditorUtility.SetDirty(@this);
+				}
+				#endif
+				component = @this.AddComponent<T>();
+			}
+			return component;
+		}
 	}
 }
