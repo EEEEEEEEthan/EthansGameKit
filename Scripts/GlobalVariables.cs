199822730a531c9cd69f410a162a7d57969c1902
ethan <ethan.tang@coconutislandstudio.com>
2023-11-07 17:05:58 +0800

    全局变量逻辑隔离

--- Scripts/GlobalVariables.cs
+++ Scripts/GlobalVariables.cs
@@ -9,12 +9,18 @@ namespace EthansGameKit
 		public static Dictionary<string, Object> Objects => GlobalObjectContainer.objects;
 		public static T Get<T>(string key) where T : Object
 		{
+#if UNITY_EDITOR
+			if (!Application.isPlaying) throw new System.InvalidOperationException("GlobalVariables.Get<T> can only be called in play mode");
+#endif
 			if (Objects.TryGetValue(key, out var result))
 				return (T)result;
 			return null;
 		}
 		public static void Set(string key, Object value)
 		{
+#if UNITY_EDITOR
+			if (!Application.isPlaying) throw new System.InvalidOperationException("GlobalVariables.Set can only be called in play mode");
+#endif
 			if (value)
 				Objects[key] = value;
 			else
--- Scripts/Internal/Singleton.cs
+++ Scripts/Internal/Singleton.cs
@@ -9,12 +9,18 @@ namespace EthansGameKit.Internal
 		{
 			get
 			{
+#if UNITY_EDITOR
+				if (!Application.isPlaying) throw new System.InvalidOperationException("Singleton<T>.Instance can only be called in play mode");
+#endif
 				if (instance) return instance;
 				instance = GlobalObjectContainer.internalObjects[typeof(T).FullName] as T;
 				return instance;
 			}
 			private set
 			{
+#if UNITY_EDITOR
+				if (!Application.isPlaying) throw new System.InvalidOperationException("Singleton<T>.Instance can only be called in play mode");
+#endif
 				instance = value;
 				GlobalObjectContainer.internalObjects[typeof(T).FullName] = value;
 			}
