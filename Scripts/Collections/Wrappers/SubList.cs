d0b23036abc53c27cb6b538da83a78d0ab1d6a9c
ethan <ethan.tang@coconutislandstudio.com>
2023-11-16 11:20:53 +0800

    rename

--- Scripts/Collections/Wrappers/SubList.cs
+++ Scripts/Collections/Wrappers/SubList.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace Game.Gameplay.TileObjects
+namespace EthansGameKit.Collections.Wrappers
 {
 	public readonly struct SubList<T> : IList<T>, IReadOnlyList<T>
 	{
