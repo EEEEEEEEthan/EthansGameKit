91ad24606550f33d7d1f08c8a630a5174d205de5
ethan <ethan.tang@coconutislandstudio.com>
2023-12-04 16:54:56 +0800

    rename

--- Scripts/SecondCounter.cs
+++ Scripts/SecondCounter.cs
@@ -2,12 +2,11 @@ using UnityEngine;
 
 namespace EthansGameKit
 {
-	public struct CountPerSecond
+	public struct SecondCounter
 	{
 		int currentCount;
 		int lastCount;
 		int lastSecond;
-		bool scaledTime;
 		public int Count
 		{
 			get
@@ -15,13 +14,6 @@ namespace EthansGameKit
 				UpdateCount();
 				return lastCount;
 			}
-		}
-		public CountPerSecond(bool useScaledTime)
-		{
-			scaledTime = useScaledTime;
-			currentCount = 0;
-			lastCount = 0;
-			lastSecond = (int)Time.time;
 		}
 		public void Add()
 		{
@@ -36,8 +28,8 @@ namespace EthansGameKit
 			{
 				lastCount = currentCount;
 				currentCount = 0;
-				lastSecond = (int)currentTime;
 			}
+			lastSecond = (int)currentTime;
 		}
 	}
 }
