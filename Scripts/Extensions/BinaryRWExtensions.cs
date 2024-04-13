fd226fb650c7cedc49bf99485bd2758bcccf737e
ethan <tyx193@live.cn>
2024-04-13 14:46:27 +0800

    extensions

--- Scripts/Extensions/BinaryRWExtensions.cs
+++ Scripts/Extensions/BinaryRWExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using EthansGameKit.CachePools;
 using UnityEngine;
@@ -6,6 +7,47 @@ using UnityEngine;
 // ReSharper disable once CheckNamespace
 namespace EthansGameKit
 {
+	readonly struct ReaderBlockScope : IDisposable
+	{
+		readonly Stream stream;
+		readonly int endPosition;
+		public ReaderBlockScope(BinaryReader reader)
+		{
+			stream = reader.BaseStream;
+			var length = reader.ReadInt32();
+			var position = reader.BaseStream.Position;
+			endPosition = (int)position + length;
+		}
+		void IDisposable.Dispose()
+		{
+			if (stream.Position > endPosition)
+			{
+				throw new("ReaderBlockScope: stream.Position > endPosition");
+			}
+			stream.Position = endPosition;
+		}
+	}
+
+	readonly struct WriterBlockScope : IDisposable
+	{
+		readonly BinaryWriter writer;
+		readonly int beginPosition;
+		public WriterBlockScope(BinaryWriter writer)
+		{
+			this.writer = writer;
+			beginPosition = (int)writer.BaseStream.Position;
+			writer.Write(0);
+		}
+		void IDisposable.Dispose()
+		{
+			var endPosition = (int)writer.BaseStream.Position;
+			var length = endPosition - beginPosition - sizeof(int);
+			writer.BaseStream.Position = beginPosition;
+			writer.Write(length);
+			writer.BaseStream.Position = endPosition;
+		}
+	}
+
 	public static partial class Extensions
 	{
 		public static void Write(this BinaryWriter @this, Vector3 vector)
@@ -65,6 +107,14 @@ namespace EthansGameKit
 			for (var i = @this.ReadInt32(); i-- > 0;)
 				queue.Enqueue(@this.ReadVector2Int());
 			return queue;
+		}
+		public static IDisposable BeginBlock(this BinaryWriter @this)
+		{
+			return new WriterBlockScope(@this);
+		}
+		public static IDisposable BeginBlock(this BinaryReader @this)
+		{
+			return new ReaderBlockScope(@this);
 		}
 	}
 }
