7cc07b38254087f49eff3658bcfc582019ad0376
ethan <tyx193@live.cn>
2024-04-22 23:42:34 +0800

    1

---
+++ Scripts/Collections/Heap.cs
@@ -0,0 +1,161 @@
+using System;
+using System.Buffers;
+
+namespace EthansGameKit.Collections
+{
+	class Heap<TKey, TValue> where TValue : IComparable<TValue>
+	{
+		TKey[] keys;
+		TValue[] values;
+		int count;
+		public Heap(int capacity)
+		{
+			keys = new TKey[capacity];
+			values = new TValue[capacity];
+		}
+		public Heap() : this(1)
+		{
+		}
+		public void Add(TKey key, TValue value)
+		{
+			if (keys.Length == count)
+			{
+				Array.Resize(ref keys, count << 1);
+				Array.Resize(ref values, count << 1);
+			}
+			keys[count] = key;
+			values[count] = value;
+			++count;
+			AdjustUp(count - 1);
+		}
+		public void Clear()
+		{
+			count = 0;
+			keys.Clear();
+			values.Clear();
+		}
+		public void TrimExcess()
+		{
+			Array.Resize(ref keys, count);
+			Array.Resize(ref values, count);
+		}
+		public bool Remove(TKey key)
+		{
+			return RemoveAt(Find(key));
+		}
+		public bool Remove(TKey key, TValue value)
+		{
+			return RemoveAt(Find(key, value));
+		}
+		public void Update(TKey key, TValue oldValue, TValue newValue)
+		{
+			Update(Find(key, oldValue), newValue);
+		}
+		public void Update(TKey key, TValue newValue)
+		{
+			Update(Find(key), newValue);
+		}
+		public int Find(TKey key)
+		{
+			for (var i = 0; i < count; ++i)
+				if (keys[i].Equals(key))
+					return i;
+			return -1;
+		}
+		public int Find(TKey key, out TValue value)
+		{
+			var index = Find(key);
+			value = index >= 0 ? values[index] : default;
+			return index;
+		}
+		public void Pop()
+		{
+			RemoveAt(0);
+		}
+		public TKey Peek()
+		{
+			return keys[0];
+		}
+		public TKey Peek(out TValue value)
+		{
+			value = values[0];
+			return keys[0];
+		}
+		public int Find(TKey key, TValue value)
+		{
+			// 用栈的方式查找，性能更好
+			var buffer = ArrayPool<int>.Shared.Rent(count / 2 + 1);
+			buffer[0] = 0;
+			var cnt = 1;
+			while (cnt > 0)
+			{
+				var currentIndex = buffer[--cnt];
+				if (currentIndex >= count) continue;
+				var currentValue = values[currentIndex];
+				var cmp = currentValue.CompareTo(value);
+				switch (cmp)
+				{
+					case 0 when keys[currentIndex].Equals(key):
+						// 键值分别相等，直接返回
+						ArrayPool<int>.Shared.Return(buffer);
+						return currentIndex;
+					case < 0:
+						// 值比节点更小，那么节点的子树全部舍弃
+						continue;
+					default:
+						// 在子树中查询
+						buffer[cnt++] = (currentIndex << 1) + 1;
+						buffer[cnt++] = (currentIndex << 1) + 2;
+						break;
+				}
+			}
+			ArrayPool<int>.Shared.Return(buffer);
+			return -1;
+		}
+		bool RemoveAt(int index)
+		{
+			if (index < 0 || index >= count) return false;
+			Swap(index, --count);
+			keys[count] = default;
+			values[count] = default;
+			AdjustDown(index);
+			AdjustUp(index);
+			return true;
+		}
+		void Update(int index, TValue newValue)
+		{
+			values[index] = newValue;
+			AdjustDown(index);
+			AdjustUp(index);
+		}
+		void Swap(int i, int j)
+		{
+			(keys[i], keys[j]) = (keys[j], keys[i]);
+			(values[i], values[j]) = (values[j], values[i]);
+		}
+		void AdjustUp(int i)
+		{
+			while (i > 0)
+			{
+				var parent = (i - 1) >> 1;
+				if (values[i].CompareTo(values[parent]) < 0) break;
+				Swap(i, parent);
+				i = parent;
+			}
+		}
+		void AdjustDown(int i)
+		{
+			while (i < count)
+			{
+				var left = (i << 1) + 1;
+				if (left >= count) break;
+				var right = left + 1;
+				var max = left;
+				if (right < count && values[right].CompareTo(values[left]) > 0) max = right;
+				if (values[i].CompareTo(values[max]) >= 0) break;
+				Swap(i, max);
+				i = max;
+			}
+		}
+	}
+}
