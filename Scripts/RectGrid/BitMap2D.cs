58f34ba38ed4f91e28f1b0e14be0f7ef51a1de32
ethan <tyx193@live.cn>
2024-04-30 00:38:13 +0800

    serialize

--- Scripts/RectGrid/BitMap2D.cs
+++ Scripts/RectGrid/BitMap2D.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
@@ -11,44 +12,74 @@ namespace EthansGameKit.RectGrid
 			chunkIndex = index >> 6;
 			bitIndex = index - (chunkIndex << 6);
 		}
+
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		static int GetIndex(int chunkIndex, int bitIndex)
-		{
-			return (chunkIndex << 6) + bitIndex;
-		}
+		static int GetIndex(int chunkIndex, int bitIndex) => (chunkIndex << 6) + bitIndex;
+
 		public readonly GridIndexCalculator Calculator;
 		readonly long[] chunks;
+
 		public BitMap2D(int widthPower)
 		{
 			Calculator = new(widthPower);
 			chunks = new long[Calculator.count >> 6];
 		}
+
+		public BitMap2D(BinaryReader reader)
+		{
+			using (reader.BeginBlock("BitMap2D"))
+			{
+				Calculator = new(reader.ReadByte());
+				chunks = new long[Calculator.count >> 6];
+				var count = chunks.Length;
+				for (var i = 0; i < count; ++i)
+					chunks[i] = reader.ReadInt64();
+			}
+		}
+
+		public void Deserialize(BinaryReader reader)
+		{
+			using (reader.BeginBlock("BitMap2D"))
+			{
+				var power = reader.ReadByte();
+				if (power != Calculator.widthPower)
+					throw new InvalidDataException($"BitMap2D power mismatch: {power} != {Calculator.widthPower}");
+				var count = chunks.Length;
+				for (var i = 0; i < count; ++i)
+					chunks[i] = reader.ReadInt64();
+			}
+		}
+
+		public void Serialize(BinaryWriter writer)
+		{
+			using (writer.BeginBlock("BitMap2D"))
+			{
+				writer.Write((byte)Calculator.widthPower);
+				var count = chunks.Length;
+				for (var i = 0; i < count; ++i)
+					writer.Write(chunks[i]);
+			}
+		}
+
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public bool GetValue(int index)
 		{
 			GetIndex(index, out var chunkIndex, out var bitIndex);
 			return (chunks[chunkIndex] & (1L << bitIndex)) != 0;
 		}
+
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public bool GetValue(int x, int y)
-		{
-			return GetValue(Calculator.GetIndex(x, y));
-		}
+		public bool GetValue(int x, int y) => GetValue(Calculator.GetIndex(x, y));
+
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public bool GetValueUnverified(int x, int y)
-		{
-			return GetValue(Calculator.GetIndexUnverified(x, y));
-		}
+		public bool GetValueUnverified(int x, int y) => GetValue(Calculator.GetIndexUnverified(x, y));
+
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public bool GetValue(Vector2Int position)
-		{
-			return GetValue(Calculator.GetIndex(position));
-		}
+		public bool GetValue(Vector2Int position) => GetValue(Calculator.GetIndex(position));
+
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public bool GetValueUnverified(Vector2Int position)
-		{
-			return GetValue(Calculator.GetIndexUnverified(position));
-		}
+		public bool GetValueUnverified(Vector2Int position) => GetValue(Calculator.GetIndexUnverified(position));
+
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public void SetValue(int index, bool value)
 		{
@@ -58,21 +89,25 @@ namespace EthansGameKit.RectGrid
 			else
 				chunks[chunkIndex] &= ~(1L << bitIndex);
 		}
+
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public void SetValue(int x, int y, bool value)
 		{
 			SetValue(Calculator.GetIndex(x, y), value);
 		}
+
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public void SetValueUnverified(int x, int y, bool value)
 		{
 			SetValue(Calculator.GetIndexUnverified(x, y), value);
 		}
+
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public void SetValue(Vector2Int position, bool value)
 		{
 			SetValue(Calculator.GetIndex(position), value);
 		}
+
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public void SetValueUnverified(Vector2Int position, bool value)
 		{
