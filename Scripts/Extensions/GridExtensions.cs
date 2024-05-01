f9bd28da1b63447761df1509215f764dfa92a791
ethan <tyx193@live.cn>
2024-05-01 23:02:55 +0800

    extensions

--- Scripts/Extensions/GridExtensions.cs
+++ Scripts/Extensions/GridExtensions.cs
@@ -15,5 +15,18 @@ namespace EthansGameKit
 			Gizmos.DrawLine(c, d);
 			Gizmos.DrawLine(d, a);
 		}
+
+		public static void Gizmos_DrawRect(this Grid grid, RectInt rect)
+		{
+			var a = grid.CellToWorld((Vector3Int)rect.min);
+			var b = grid.CellToWorld((Vector3Int)rect.min + Vector3Int.right * rect.width);
+			var c = grid.CellToWorld((Vector3Int)rect.min + Vector3Int.right * rect.width +
+			                         Vector3Int.up * rect.height);
+			var d = grid.CellToWorld((Vector3Int)rect.min + Vector3Int.up * rect.height);
+			Gizmos.DrawLine(a, b);
+			Gizmos.DrawLine(b, c);
+			Gizmos.DrawLine(c, d);
+			Gizmos.DrawLine(d, a);
+		}
 	}
 }
