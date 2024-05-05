fa079d4efd2bb627dd6be851a8d02de9cac76a6b
ethan <tyx193@live.cn>
2024-05-05 18:56:09 +0800

    extensions

--- Scripts/Extensions/Int32Extensions.cs
+++ Scripts/Extensions/Int32Extensions.cs
@@ -1,10 +1,20 @@
 namespace EthansGameKit
 {
-    public static partial class Extensions
-    {
-        public static float Sqrt(this int @this)
-        {
-            return (float)System.Math.Sqrt(@this);
-        }
-    }
+	public static partial class Extensions
+	{
+		public static float Sqrt(this int @this)
+		{
+			return (float)System.Math.Sqrt(@this);
+		}
+
+		public static int Clamped(this int @this, int min, int max)
+		{
+			return @this < min ? min : @this > max ? max : @this;
+		}
+
+		public static void Clamp(ref this int @this, int min, int max)
+		{
+			@this = @this < min ? min : @this > max ? max : @this;
+		}
+	}
 }
