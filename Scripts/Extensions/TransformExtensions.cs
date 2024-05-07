6db1720dcff363fdabfcb1a96b2cc6bfa0d1f7c8
ethan <ethan.tang@coconutislandstudio.com>
2024-05-07 11:50:50 +0800

    1

--- Scripts/Extensions/TransformExtensions.cs
+++ Scripts/Extensions/TransformExtensions.cs
@@ -7,49 +7,5 @@ namespace EthansGameKit
 {
 	public static partial class Extensions
 	{
-		struct DfsChildrenEnumerator : IEnumerator<Transform>, IEnumerable<Transform>
-		{
-			readonly Stack<Transform> stack;
-			Transform root;
-
-			public DfsChildrenEnumerator(Transform root)
-			{
-				this.root = root;
-				stack = null;
-			}
-
-			public Transform Current => stack.Peek();
-
-			object IEnumerator.Current => Current;
-
-			public bool MoveNext()
-			{
-				if (!stack.TryPop(out var element))
-				{
-					return false;
-				}
-				stack.
-			}
-
-			public void Reset()
-			{
-				throw new NotImplementedException();
-			}
-
-			public void Dispose()
-			{
-				throw new NotImplementedException();
-			}
-
-			public IEnumerator<Transform> GetEnumerator()
-			{
-				throw new NotImplementedException();
-			}
-
-			IEnumerator IEnumerable.GetEnumerator()
-			{
-				return GetEnumerator();
-			}
-		}
 	}
 }
