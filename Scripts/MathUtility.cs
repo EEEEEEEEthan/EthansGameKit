6534d1c44b86577684da58f458ac8e1a6374fd6c
ethan <ethan.tang@coconutislandstudio.com>
2024-05-14 14:34:21 +0800

    public

--- Scripts/MathUtility.cs
+++ Scripts/MathUtility.cs
@@ -4,7 +4,7 @@ namespace EthansGameKit
 {
 	public static class MathUtility
 	{
-		static void Hermite(float p0, float v0, float p1, float v1, float progress, out float point, out float speed)
+		public static void Hermite(float p0, float v0, float p1, float v1, float progress, out float point, out float speed)
 		{
 			var t1 = progress;
 			var t2 = t1 * t1;
@@ -15,7 +15,7 @@ namespace EthansGameKit
 					p1 * (-6 * t2 + 6 * t1) +
 					v1 * (3 * t2 - 2 * t1);
 		}
-		static void Hermite(Vector2 p0, Vector2 v0, Vector2 p1, Vector2 v1, float progress, out Vector2 point, out Vector2 speed)
+		public static void Hermite(Vector2 p0, Vector2 v0, Vector2 p1, Vector2 v1, float progress, out Vector2 point, out Vector2 speed)
 		{
 			var t1 = progress;
 			var t2 = t1 * t1;
@@ -26,7 +26,7 @@ namespace EthansGameKit
 					p1 * (-6 * t2 + 6 * t1) +
 					v1 * (3 * t2 - 2 * t1);
 		}
-		static void Hermite(Vector3 p0, Vector3 v0, Vector3 p1, Vector3 v1, float progress, out Vector3 point, out Vector3 speed)
+		public static void Hermite(Vector3 p0, Vector3 v0, Vector3 p1, Vector3 v1, float progress, out Vector3 point, out Vector3 speed)
 		{
 			var t1 = progress;
 			var t2 = t1 * t1;
