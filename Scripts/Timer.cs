87df18edac3b2101f7906668ca1eb165f5a08bbb
ethan <tyx193@live.cn>
2024-06-09 22:18:51 +0800

    timer

--- Scripts/Timer.cs
+++ Scripts/Timer.cs
@@ -3,24 +3,36 @@ using EthansGameKit.Internal;
 
 namespace EthansGameKit
 {
-	public static class Timer
-	{
-		public static uint InvokeAfter(double seconds, Action callback) =>
-			MainThreadTimer.updater.InvokeAfter(seconds, callback);
+    public static class Timer
+    {
+        public static uint InvokeAfter(double seconds, Action callback)
+        {
+            return MainThreadTimer.updater.InvokeAfter(seconds, callback);
+        }
 
-		public static void InvokeAfter(ref uint id, double seconds, Action callback) =>
-			MainThreadTimer.updater.InvokeAfter(ref id, seconds, callback);
+        public static void InvokeAfter(ref uint id, double seconds, Action callback)
+        {
+            MainThreadTimer.updater.InvokeAfter(ref id, seconds, callback);
+        }
 
-		public static IAwaitable Await(double seconds) =>
-			MainThreadTimer.updater.Await(seconds);
+        public static IAwaitable Await(double seconds)
+        {
+            return MainThreadTimer.updater.Await(seconds);
+        }
 
-		public static IAwaitable Await(ref uint id, double seconds) =>
-			MainThreadTimer.updater.Await(ref id, seconds);
+        public static IAwaitable Await(ref uint id, double seconds)
+        {
+            return MainThreadTimer.updater.Await(ref id, seconds);
+        }
 
-		public static IAwaitable Await(double seconds, out uint id) =>
-			MainThreadTimer.updater.Await(seconds, out id);
+        public static IAwaitable Await(double seconds, out uint id)
+        {
+            return MainThreadTimer.updater.Await(seconds, out id);
+        }
 
-		public static bool CancelInvoke(uint id) =>
-			MainThreadTimer.updater.CancelInvoke(id);
-	}
+        public static bool CancelInvoke(uint id)
+        {
+            return MainThreadTimer.updater.CancelInvoke(id);
+        }
+    }
 }
